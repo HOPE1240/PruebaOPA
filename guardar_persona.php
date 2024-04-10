74cc6c0e52edeb7d3907aa824d9e12452557fa30
santiago <uribesantiago12@gmail.com>
2024-04-09 19:21:03 -0500

    PrimeraV

---
+++ guardar_persona.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $nombre = isset($_POST["nombre"]) ? $_POST["nombre"] : "";
+  $edad = isset($_POST["edad"]) ? $_POST["edad"] : "";
+
+  if (!empty($nombre) && !empty($edad)) {
+    // Guardar los datos en el archivo personas.txt
+    $archivo = fopen("personas.txt", "a");
+    fwrite($archivo, "$nombre, $edad\n");
+    fclose($archivo);
+
+    echo "<p class='mt-4'>¡Registro exitoso! Se ha guardado la siguiente información: Nombre: $nombre, Edad: $edad</p>";
+  } else {
+    echo "<p class='mt-4'>No se han proporcionado datos válidos para el registro. Por favor, ingrese nombre y edad válidos.</p>";
+  }
+}
+?>
